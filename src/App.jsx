602b386518d208fe0bf361bb057fc02447fb52f6
Kawann <kawannmarcelo7@gmail.com>
2025-05-07 20:36:52 -0300

    feat: wrapping the application with the AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </AuthProvider>
   )
 }
 
