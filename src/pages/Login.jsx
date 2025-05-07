38a892ce6a591288c5b3180ea30110b918eccb09
Kawann <kawannmarcelo7@gmail.com>
2025-05-07 20:06:11 -0300

    feat: adding login function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,39 @@
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
+import { toast, ToastContainer } from "react-toastify";
 
 export default function Login() {
+    const url = "http://localhost:8080";
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     const navigate = useNavigate();
 
     const goToRegister = () => {
         navigate("/register")
+    }
+
+    const handleLogin = async () => {
+        if (!email || !password) {
+            toast.error("Preencha todos os campos");
+            return
+        }
+
+        try {
+            await axios.post(`${url}/invite/login`, {
+                email,
+                password
+            })
+            toast.success("Login realizado com sucesso!");
+            setTimeout(() => {
+                navigate("/home");
+            }, 1000);
+        } catch (error) {
+            console.log(error);
+            toast.error("Erro ao realizar login!");
+        }
     }
 
     return (
@@ -17,14 +46,26 @@ export default function Login() {
                 Não Possui uma Conta ?
             </button>
             <div className="flex flex-col items-center justify-center">
-                <input type="email" placeholder="Digite seu Email"/>
-                <input type="password" placeholder="Digite sua Senha"/>
+                <input 
+                    type="email" 
+                    placeholder="Digite seu Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Digite sua Senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <button
+                    onClick={handleLogin}
                     className="cursor-pointer"
                 >
                     ENTRAR
                 </button>
             </div>
+            <ToastContainer />
         </div>
     )
 }
